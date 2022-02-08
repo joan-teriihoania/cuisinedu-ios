a167a5f0447e240c17150c0462bfc4b645ee6c75
Vincent-Baret1 <vincent.baret@etu.umontpellier.fr>
2022-02-08 08:24:59 +0100

    Initial Commit

---
+++ Test_Cours_listUI/Test_Cours_listUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Cours_listUIApp.swift
+//  Test_Cours_listUI
+//
+//  Created by Vincent Baret on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Test_Cours_listUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
